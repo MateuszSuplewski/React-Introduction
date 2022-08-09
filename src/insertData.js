42cbfb1c68cfa89582963008572f6cc403792b3c
Mateusz Suplewski <matx3582@gmail.com>
2022-08-09 11:36:02 +0200

    Exported insert data

---
+++ src/insertData.js
@@ -0,0 +1,52 @@
+const insertData = {
+    items: [
+      { section: 'Home', url: '/' },
+      { section: 'About us', url: '/about' },
+      { section: 'Get in touch', url: '/contact' }
+    ],
+  
+    logoParams: {
+      url: 'https://ftsdlskits.com/wp-content/uploads/2018/12/Paris-Saint-Germain-PSG-Logo-512%C3%97512-URL.png',
+      alt: 'Paris Saint Germain PSG Logo'
+    },
+    copyrightsText: 'Copyright [Icon] All rights reserved by Mateusz Suplewski - @2022',
+  
+    post: {
+      title: 'Hello React',
+      body: 'Lorem ipsum....',
+      author: 'Mirek Lorek',
+      created: '2019-01-01 01:02:03'
+    },
+  
+    posts: [{
+      title: 'Hello React 1',
+      body: 'Lorem ipsum....',
+      author: 'Mirek Lorek',
+      created: '2019-01-01 01:02:03'
+    },
+  
+    {
+      title: 'Hello React 2',
+      body: 'Lorem ipsum....',
+      author: 'Mirek Lorek',
+      created: '2019-01-01 01:02:03'
+    },
+  
+    {
+      title: 'Hello React 3',
+      body: 'Lorem ipsum....',
+      author: 'Mirek Lorek',
+      created: '2019-01-01 01:02:03'
+    }
+    ],
+  
+    galleryItems: [
+      { url: 'https://i.picsum.photos/id/312/200/300.jpg?hmac=bU-4jjrQQCaBqT1RvbRjEZcTQiUWLY5Hp4vYkD5xIKw', created: '2009-01-01 05:02:43', author: 'Mateusz Suplewski' },
+      { url: 'https://i.picsum.photos/id/62/200/300.jpg?hmac=Ova5b3XqMVygL4ZvFJ1MfAehiXKiM1Ol14jN_6widUY', created: '2018-03-05 07:02:43', author: 'Kacper Uska' },
+      { url: 'https://i.picsum.photos/id/977/200/300.jpg?hmac=YYtcm39X8v9y0KYAb_9s-ufIz_R0Kgbt_EP0F8-jkFU', created: '2009-11-11 14:22:43', author: 'Norbert Gier' },
+      { url: 'https://i.picsum.photos/id/1012/200/300.jpg?hmac=KU5TJQJkcv2lK_5lVNCie4evqxUOfFGp0Qsv2gQZo5k', created: '2005-02-01 05:22:55', author: 'Adam Kowalski' },
+      { url: 'https://i.picsum.photos/id/658/200/300.jpg?hmac=K1TI0jSVU6uQZCZkkCMzBiau45UABMHNIqoaB9icB_0', created: '2003-01-014 14:22:43', author: 'Paulina Szolt' }
+    ]
+  }
+
+  export default insertData
